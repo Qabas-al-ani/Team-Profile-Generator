985009fefaebec664918e48ff5be434a3c2199c4
Qabas al ani <doctorqa@yahoo.com>
2021-08-08 18:37:11 -0400

    creted constructor for employee

---
+++ index.js
@@ -0,0 +1,7 @@
+// link to page creation
+const generateHTML = require('./src/generateHTML');
+
+// team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
---
+++ test/Employee.test.js
@@ -0,0 +1,60 @@
+// using Employee constructor
+const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+  // creates employee object
+  it("creates an employee object", () => {
+    const name = "Qabas";
+    const id = 1981;
+    const email = "QabasDeveloper@gmail.com";
+
+    const employee = new Employee(name, id, email);
+
+    expect(employee.name).toEqual(name);
+    expect(employee.id).toEqual(id);
+    expect(employee.email).toEqual(email);
+  });
+  // get name from getName method
+  it("gets employee name", () => {
+    const name = "Qabas";
+    const id = 1981;
+    const email = "QabasDeveloper@gmail.com";
+
+    const employee = new Employee(name, id, email);
+
+    expect(employee.getName()).toEqual(name);
+  });
+
+  // gets id from getId method
+  it("gets employee ID", () => {
+    const name = "Qabas";
+    const id = 1981;
+    const email = "QabasDeveloper@gmail.com";
+
+    const employee = new Employee(name, id, email);
+
+    expect(employee.getId()).toEqual(id);
+  });
+
+  // gets emails from getEmail method
+  it("gets employee email", () => {
+    const name = "Qabas";
+    const id = 1981;
+    const email = "QabasDeveloper@gmail.com";
+
+    const employee = new Employee(name, id, email);
+
+    expect(employee.getEmail()).toEqual(email);
+  });
+
+  // gets role from getRole method
+  it("gets role of employee", () => {
+    const name = "Qabas";
+    const id = 1981;
+    const email = "QabasDeveloper@gmail.com";
+
+    const employee = new Employee(name, id, email);
+
+    expect(employee.getRole()).toEqual("Employee");
+  });
+});
