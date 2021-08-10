02dc56fa20c7e48ddd17866ea38290320dab3f42
Qabas al ani <doctorqa@yahoo.com>
2021-08-09 22:19:14 -0400

    created the index HTML

--- index.js
+++ index.js
@@ -49,72 +49,97 @@ const addManager = () => {
 
 // adding employee section
 const addEmployee = () => {
-   return inquirer.prompt ([
-        {
-            type: 'list',
-            name: 'role',
-            message: "Please choose your employee's role",
-            choices: ['Engineer', 'Intern']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: "What's the name of the employee?", 
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Please enter the employee's ID.",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Please enter the employee's email.",
-          
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: "Please enter the employee's github username.",
-        
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Please enter the intern's school",
-        
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddEmployee',
-            message: 'Would you like to add more team members?',
-            default: false
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Please choose your employee's role",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What's the name of the employee?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the employee's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the employee's email.",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter the employee's github username.",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter the intern's school",
+      },
+      {
+        type: "confirm",
+        name: "confirmAddEmployee",
+        message: "Would you like to add more team members?",
+        default: false,
+      },
     ])
-    .then(employeeData => {
-        // data for employee types 
+    .then((employeeData) => {
+      // data for employee types
 
-        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
-        let employee; 
+      let { name, id, email, role, github, school, confirmAddEmployee } =
+        employeeData;
+      let employee;
 
-        if (role === "Engineer") {
-            employee = new Engineer (name, id, email, github);
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
 
-            console.log(employee);
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
 
-        } else if (role === "Intern") {
-            employee = new Intern (name, id, email, school);
+        console.log(employee);
+      }
 
-            console.log(employee);
-        }
+      teamArray.push(employee);
 
-        teamArray.push(employee); 
-
-        if (confirmAddEmployee) {
-            return addEmployee(teamArray); 
-        } else {
-            return teamArray;
-        }
-    })
-
+      if (confirmAddEmployee) {
+        return addEmployee(teamArray);
+      } else {
+        return teamArray;
+      }
+    });
 };
+
+// function to generate HTML page file using file system
+const writeFile = (data) => {
+  fs.writeFile("./dist/index.html", data, (err) => {
+    // if there is an error
+    if (err) {
+      console.log(err);
+      return;
+      // when the profile has been created
+    } else {
+      console.log(
+        "Your team profile has been successfully created! Please check out the index.html"
+      );
+    }
+  });
+};
+
+addManager()
+  .then(addEmployee)
+  .then((teamArray) => {
+    return generateHTML(teamArray);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
